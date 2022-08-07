d0df9cf2cc5b3ed4b081172af11571da6b8d1939
Sumit Shrestha <summitshrestha8@gmail.com>
2022-08-07 12:43:59 -0500

    a tag

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,11 @@ import React from "react";
 function Articles(props) {
   return (
     <article>
-      <a href={"http://reddit.com" + props.article.permalink} target="_blank">
+      <a
+        href={"http://reddit.com" + props.article.permalink}
+        target="_blank"
+        rel="noreferrer"
+      >
         <h3>{props.article.title}</h3>
       </a>
     </article>
