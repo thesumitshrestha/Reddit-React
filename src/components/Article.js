2ae93a093197985e1ff97524f3541ee7bdfa07a3
Sumit Shrestha <summitshrestha8@gmail.com>
2022-08-07 12:34:32 -0500

    Reddit React

---
+++ src/components/Article.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Articles(props) {
+  return (
+    <article>
+      <a href={"http://reddit.com" + props.article.permalink} target="_blank">
+        <h3>{props.article.title}</h3>
+      </a>
+    </article>
+  );
+}
+
+export default Articles;
